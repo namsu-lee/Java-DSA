bd88c8c8e34131edb7ad18c75bf9b18e3ef9a5b1
namsu <dlskatnekrh@gmail.com>
2025-03-28 19:14:37 +0900

    연결 리스트 구현 추가(작성 중)

---
+++ 05.LinkedList/LinkedList.java
@@ -0,0 +1,49 @@
+public class LinkedList {
+    private Node head;
+    private Node tail;
+    private int length;
+
+    public LinkedList(int value) {
+        Node newNode = new Node(value);
+        this.head = newNode;
+        this.tail = newNode;
+        this.length = 1;
+    }
+
+    public void printList() {
+        Node temp = head;
+
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (this.length == 0) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+    }
+
+    public void prepend(int value) {
+    }
+
+    public boolean insert(int index, int value) {
+        return true;
+    }
+
+    class Node {
+        int value;
+        Node next = null;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+}
